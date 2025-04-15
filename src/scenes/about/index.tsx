ead797dcc108e5d15382e2f78aa5460569b21008
Elisaatnam <elisa.stueker@googlemail.com>
2025-04-15 17:01:49 +0200

    quick mobile layout fix

--- src/scenes/about/index.tsx
+++ src/scenes/about/index.tsx
@@ -10,16 +10,18 @@ function About({ setSelectedPage }: AboutProps) {
   return (
     <motion.section
       id="about"
-      className="relative overflow-clip md:min-h-screen"
+      className="relative min-h-screen overflow-clip"
       onViewportEnter={() => setSelectedPage(SelectedPage.UeberMich)}
       viewport={{ once: true, amount: 0.5 }}
     >
       <img
         src={SideProfile}
         alt="Seitenprofil Jan Reiche"
-        className="absolute h-full w-full scale-125 object-cover blur-sm"
+        className="absolute h-full w-full object-cover object-left blur-sm md:scale-125 md:object-center"
       />
-      <h1 className="absolute z-10">Hello</h1>
+      <h2 className="absolute top-1/2 right-1/2 bottom-1/2 left-1/2 z-10">
+        About...
+      </h2>
     </motion.section>
   );
 }
